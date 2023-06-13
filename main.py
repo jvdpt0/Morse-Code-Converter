5c78179502c1ee4e1c5064d0bf1ec7e9031fc27a
jvdpt0 <jvdpt0@gmail.com>
2023-06-13 15:48:23 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+MORSE_DICT = {
+    'A':'.-',
+    'B':'-...',
+    'C':'-.-.',
+    'D':'-..',
+    'E':'.',
+    'F':'..-.',
+    'G':'--.',
+    'H':'....',
+    'I':'..',
+    'J':'.---',
+    'K':'-.-',
+    'L':'.-..',
+    'M':'--',
+    'N':'-.',
+    'O':'---',
+    'P':'.--.',
+    'Q':'--.-',
+    'R':'.-.',
+    'S':'...',
+    'T':'-',
+    'U':'..-',
+    'V':'...-',
+    'W':'.--',
+    'X':'-..-',
+    'Y':'-.--',
+    'Z':'--..',
+    '1':'.----',
+    '2':'..---',
+    '3':'...--',
+    '4':'....-',
+    '5':'.....',
+    '6':'-....',
+    '7':'--...',
+    '8':'---..',
+    '9':'----.',
+    '0':'-----'
+}
+
+string_to_convert = input('Insert a string you want to convert to morse code: ').upper()
+morse_code_string=''
+
+for element in string_to_convert:
+    if element == ' ':
+        morse_code_string += ' '
+    else:
+        morse_code_string += MORSE_DICT[element]+' '
+
+print(f'This is your morse code string: {morse_code_string}')
